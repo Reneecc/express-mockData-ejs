a132588e27aa8cb2668c5c92f20a722d121e0829
CCRENEE <baorui@dmallmax.com>
2021-01-19 14:33:00 +0800

    feat: 新增树形图解析和app.js注释

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var express = require('express'); //加载express模块
+var path = require('path'); //路径模块
+var cookieParser = require('cookie-parser'); //解析cookie的工具，通过req.cookies可以取到传过来的cookie，并把它们转成对象
+var logger = require('morgan'); //日志中间件，能够在控制台中，显示req请求的信息
 // var request = require('request')
 
+//路由信息（接口地址），存放在routes的根目录
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var shopcarRouter = require('./routes/shopcar');
@@ -14,22 +15,26 @@ var localRouter = require('./routes/localData')
 var app = express();
 
 // view engine setup 注册模版引擎 ejs
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views')); //设置视图根目录 __dirname指向被执行文件的绝对路径
+app.set('view engine', 'ejs'); //设置视图格式
 
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+//载入中间件
+app.use(logger('dev')); //日志中间件
+app.use(express.json()); //解析json的中间件
+app.use(express.urlencoded({ extended: false })); //解析urlencoded请求体的中间件
+app.use(cookieParser()); //解析cookie的中间件
+
+//设置public文件夹为存放静态文件的目录
 app.use(express.static(path.join(__dirname, 'public')));//引入静态文件
 
+//配置路由 （'自定义路径',上面设置的接口地址）
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/shopcar', shopcarRouter);
 app.use('/lagouTest', lagouRouter);
 app.use('/localTest',localRouter)
 
-// catch 404 and forward to error handler
+// catch 404 and forward to error handler  错误处理
 app.use(function (req, res, next) {
   next(createError(404));
 });
---
+++ tree.md
@@ -0,0 +1,28 @@
+.
+├── app.js                        启动文件（入口文件）
+├── bin                           存放可执行文件，如默认端口3000
+│   └── www
+├── package-lock.json 
+├── package.json                  存储工程的信息即及模块依赖，npm i ,下载dependencies中的所有依赖
+├── public                        存放image、css、js等文件
+│   ├── images
+│   ├── javascripts
+│   ├── mydata
+│   │   └── carData.json
+│   └── stylesheets
+│       └── style.css
+├── routes                        存放路由文件
+│   ├── index.js
+│   ├── lagou.js
+│   ├── localData.js
+│   ├── shopcar.js
+│   └── users.js
+├── tree.md                       树形输出图文件
+└── views                         存放视图（模版文件）
+    ├── error.ejs
+    ├── index.ejs
+    ├── lagouData.ejs
+    ├── localData.ejs
+    └── testcar.ejs
+
+8 directories, 17 files
